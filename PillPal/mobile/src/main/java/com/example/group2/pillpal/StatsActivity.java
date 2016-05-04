0557c57561ad855688c038f88ba5a569bdd1dd7f
Hamza Ahmed <hamza.ah@berkeley.edu>
2016-05-04 12:26:33 -0700

    Changed navbar icons

--- PillPal/mobile/src/main/java/com/example/group2/pillpal/StatsActivity.java
+++ PillPal/mobile/src/main/java/com/example/group2/pillpal/StatsActivity.java
@@ -67,9 +67,6 @@ public class StatsActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
