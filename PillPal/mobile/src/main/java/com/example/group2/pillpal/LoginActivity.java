2682ce75f97fe07091d6eee5d908ec3d663e5af3
Hamza Ahmed <hamza.ah@berkeley.edu>
2016-05-03 23:10:27 -0700

    Fixed issue with login

--- PillPal/mobile/src/main/java/com/example/group2/pillpal/LoginActivity.java
+++ PillPal/mobile/src/main/java/com/example/group2/pillpal/LoginActivity.java
@@ -84,7 +84,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        createUsers();
+        this.createUsers();
 
 //        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
 //        mEmailSignInButton.setOnClickListener(new OnClickListener() {
