b59fdb7453ca92be79cabf1e15934a66d27a4331
Unzi Park <upark.go@gmail.com>
2016-05-06 00:31:02 -0700

    Fix before presentation

--- PillPal/wear/src/main/java/com/example/group2/pillpal/StatFragment.java
+++ PillPal/wear/src/main/java/com/example/group2/pillpal/StatFragment.java
@@ -118,8 +118,7 @@ public class StatFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+//            throw new RuntimeException(context.toString());
         }
     }
 
